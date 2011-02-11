ff1f035f9c2b30e8f842a20df9a8e5c0172bff1a
Brandon Sneed <brandon@redf.net>
2011-02-11 15:26:35 -0800

    added some existing classes.

---
+++ UI/NSObject+SDExtensions.h
@@ -0,0 +1,21 @@
+//
+//  NSObject+SDExtensions.h
+//
+//  Created by brandon on 1/14/11.
+//  Copyright 2011 Set Direction. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// makes loading nibs much easier.
+
+@interface NSObject (NSObject_SDExtensions)
+
++ (NSString *)className;
+- (NSString *)className;
+
++ (NSString *)nibName;
++ (id)loadFromNib;
++ (id)loadFromNibWithOwner:(id)owner;
+
+@end
---
+++ UI/NSObject+SDExtensions.m
@@ -0,0 +1,48 @@
+//
+//  NSObject+SDExtensions.m
+//  billingworks
+//
+//  Created by brandon on 1/14/11.
+//  Copyright 2011 Set Direction. All rights reserved.
+//
+
+#import "NSObject+RFExtensions.h"
+
+
+@implementation NSObject (NSObject_RFExtensions)
+
++ (NSString *)className
+{
+    return NSStringFromClass(self);
+}
+
+- (NSString *)className
+{
+    return [[self class] className];
+}
+
++ (NSString *)nibName
+{
+    return [self className];
+}
+
++ (id)loadFromNib
+{
+    return [self loadFromNibWithOwner:self];
+}
+
++ (id)loadFromNibWithOwner:(id)owner
+{
+    NSArray *objects = [[NSBundle mainBundle] loadNibNamed:[self nibName] owner:owner options:nil];
+	for (id object in objects)
+    {
+		if ([object isKindOfClass:self])
+			return object;
+	}
+    
+	NSString *warning = [NSString stringWithFormat:@"Could not find object of class %@ in nib %@", [self class], [self nibName]];
+	NSAssert(NO, warning);
+	return nil;
+}
+
+@end
