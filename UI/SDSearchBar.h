fff670dce0252fa195830b481b4d81c6afb5deb1
Brandon Sneed <brandon@redf.net>
2011-02-23 15:04:01 -0800

    added SDSearchBar

---
+++ UI/SDSearchBar.h
@@ -0,0 +1,25 @@
+//
+//  SDSearchBar.h
+//  walmart
+//
+//  Created by brandon on 2/23/11.
+//  Copyright 2011 Set Direction. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SDSearchBar : UISearchBar
+{
+	UIViewController *controller;
+	UIView *obscuringView;
+	BOOL isActive;
+}
+
+@property (nonatomic, assign) BOOL active;
+@property (nonatomic, assign) IBOutlet UIViewController *controller;
+@property (nonatomic, readonly) UIView *obscuringView;
+
+- (void)setActive:(BOOL)active animated:(BOOL)animated;
+
+@end
---
+++ UI/SDSearchBar.m
@@ -0,0 +1,76 @@
+//
+//  SDSearchBar.m
+//  walmart
+//
+//  Created by brandon on 2/23/11.
+//  Copyright 2011 Set Direction. All rights reserved.
+//
+
+#import "SDSearchBar.h"
+
+
+@implementation SDSearchBar
+
+@synthesize controller;
+@synthesize active = isActive;
+
+- (void)dealloc
+{
+	[obscuringView release];
+	[controller release];
+    [super dealloc];
+}
+
+- (UIView *)obscuringView
+{
+	if (!obscuringView && controller)
+	{
+		CGRect rect = controller.view.frame;
+		
+		obscuringView = [[UIView alloc] initWithFrame:rect];
+		obscuringView.backgroundColor = [UIColor blackColor];
+		obscuringView.opaque = NO;
+		obscuringView.alpha = 0;
+		obscuringView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+	}
+	
+	return obscuringView;
+}
+
+- (void)setActive:(BOOL)active
+{
+	[self setActive:active animated:YES];
+}
+
+- (void)setActive:(BOOL)active animated:(BOOL)animated;
+{
+	if (!isActive)
+	{
+		[controller.view addSubview:self.obscuringView];
+		[controller.view bringSubviewToFront:self];
+		[UIView animateWithDuration:0.2 animations:^{
+			self.obscuringView.alpha = 0.75;
+		}];
+		[controller.navigationController setNavigationBarHidden:YES animated:animated];
+		[self setShowsCancelButton:YES animated:animated];
+		isActive = YES;
+	}
+	else
+	{
+		[controller.view addSubview:self.obscuringView];
+		[UIView animateWithDuration:0.2 
+						 animations:^{
+							 self.obscuringView.alpha = 0;
+						 }
+						 completion:^(BOOL finished){
+							 //if (finished)
+							 //	 [self.obscuringView removeFromSuperview];
+						 }];
+		[controller.navigationController setNavigationBarHidden:NO animated:animated];
+		[self setShowsCancelButton:NO animated:animated];
+		[self resignFirstResponder];
+		isActive = NO;
+	}
+}
+
+@end
