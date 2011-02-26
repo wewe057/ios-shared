6540168caccb40d68df147dbbb705f4bc25a560e
Ben Galbraith <ben@galbraiths.org>
2011-02-25 17:38:22 -0800

    HTML escaping category

---
+++ Foundation/NSString+SDExtensions.h
@@ -0,0 +1,16 @@
+//
+//  NSString+SDExtensions.h
+//  walmart
+//
+//  Created by Ben Galbraith on 2/25/11.
+//  Copyright 2011 Set Direction. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSString(SDExtensions)
+
+- (NSString *)replaceHTMLWithUnformattedText;
+
+@end
---
+++ Foundation/NSString+SDExtensions.m
@@ -0,0 +1,21 @@
+//
+//  NSString+SDExtensions.m
+//  walmart
+//
+//  Created by Ben Galbraith on 2/25/11.
+//  Copyright 2011 Set Direction. All rights reserved.
+//
+
+#import "NSString+SDExtensions.h"
+
+
+@implementation NSString(SDExtensions)
+
+- (NSString *)replaceHTMLWithUnformattedText {
+    NSString* fixed = self;
+    fixed = [fixed stringByReplacingOccurrencesOfString:@"&quot;" withString:@"\""];
+    fixed = [fixed stringByReplacingOccurrencesOfString:@"&apos;" withString:@"'"];
+    return fixed;
+}
+
+@end
