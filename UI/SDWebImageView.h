5b7dac18c218535926ddfa2a1b597c66eaa844f5
Sam Grover <samgrover@gmail.com>
2011-02-28 21:54:59 -0800

    Added SDWebImageView, a subclass of UIImageView that retrieves and updates its image.

---
+++ UI/SDWebImageView.h
@@ -0,0 +1,20 @@
+//
+//  SDWebImageView.h
+//  walmart
+//
+//  Created by Sam Grover on 2/28/11.
+//  Copyright 2011 Set Direction. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SDWebImageView : UIImageView {
+	NSString *imageUrlString;
+	UIImage *errorImage;
+}
+
+@property (nonatomic, copy) NSString *imageUrlString;
+@property (nonatomic, copy) UIImage *errorImage;
+
+@end
