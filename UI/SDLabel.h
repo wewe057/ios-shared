f3dc87008f1d73bd124dfe3e9ca963d12c58565e
Sam Grover <samgrover@gmail.com>
2011-03-05 19:21:03 -0800

    Created SDLabel, A subclass of UILabel that adjusts it's height for multi-line text.

---
+++ UI/SDLabel.h
@@ -0,0 +1,16 @@
+//
+//  SDLabel.h
+//  walmart
+//
+//  Created by Sam Grover on 3/5/11.
+//  Copyright 2011 Set Direction. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SDLabel : UILabel {
+    
+}
+
+@end
