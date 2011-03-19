cd64db626ef97c4934ffcccb787e59572ccebe20
Sam Grover <samgrover@gmail.com>
2011-03-19 14:57:41 -0700

    A simple color convenience method in an extension.

---
+++ UI/UIColor+SDExtensions.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+SDExtensions.h
+//  walmart
+//
+//  Created by Sam Grover on 3/19/11.
+//  Copyright 2011 Set Direction. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface UIColor (SDExtensions)
+
++ (UIColor *)colorWith8BitRed:(NSUInteger)red green:(NSUInteger)green blue:(NSUInteger)blue alpha:(CGFloat)alpha;
+
+@end
