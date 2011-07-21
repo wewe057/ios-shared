c9ee1d8341e99af72a7d2f71668c80a9e3c66a40
Brandon Sneed <brandon@redf.net>
2011-07-21 12:37:52 -0700

    Added SDImageButton

---
+++ UI/SDImageButton.h
@@ -0,0 +1,16 @@
+//
+//  SDImageButton.h
+//  ipad
+//
+//  Created by Brandon Sneed on 7/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDImageButton : UIControl
+
+@property (nonatomic, retain) UIImage *image;
+@property (nonatomic, retain) UIImage *selectedImage;
+
+@end
---
+++ UI/SDImageButton.m
@@ -0,0 +1,68 @@
+//
+//  SDImageButton.m
+//  ipad
+//
+//  Created by Brandon Sneed on 7/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SDImageButton.h"
+
+@implementation SDImageButton
+
+@synthesize image;
+@synthesize selectedImage;
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    
+    self.userInteractionEnabled = YES;
+    self.clipsToBounds = YES;
+    
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected
+{
+    [super setSelected:selected];
+    [self setNeedsDisplay];
+}
+
+- (void)setFrame:(CGRect)frame
+{
+    [super setFrame:frame];
+    [self setNeedsDisplay];
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    if (self.selected)
+        [selectedImage drawAtPoint:CGPointMake(0, 0)];
+    else
+        [image drawAtPoint:CGPointMake(0, 0)];//InRect:CGRectMake(0, 0, image.size.width, image.size.height)];
+}
+
+- (BOOL)beginTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    BOOL result = [super beginTrackingWithTouch:touch withEvent:event];
+    
+    self.selected = YES;
+    
+    return result;
+}
+
+- (BOOL)continueTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    self.selected = NO;
+    return NO;
+}
+
+- (void)endTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    [super endTrackingWithTouch:touch withEvent:event];
+    
+    self.selected = NO;
+}
+
+@end
