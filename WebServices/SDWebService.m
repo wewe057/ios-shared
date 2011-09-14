dd7b36eb2d2e8943dc4098eda6f517e7888ede21
Sam Grover <samgrover@gmail.com>
2011-09-14 15:04:44 -0700

    Support for 302 redirect handler.

--- WebServices/SDWebService.m
+++ WebServices/SDWebService.m
@@ -98,6 +98,11 @@
     return [[Reachability reachabilityForInternetConnection] isReachable];
 }
 
+- (void)will302RedirectToUrl:(NSURL *)argUrl
+{
+	// Implement in service subclass for specific behavior
+}
+
 - (void)clearCache
 {
 	[[ASIDownloadCache sharedCache] clearCachedResponsesForStoragePolicy:ASICachePermanentlyCacheStoragePolicy];
@@ -329,6 +334,12 @@
 		completionBlock([request responseStatusCode], responseString, &error);
         [request setCompletionBlock:nil];
         [request setFailedBlock:nil];
+	}];
+	
+	[request setRequestRedirectedBlock:^{
+		if (([request responseStatusCode] == 302)) {
+			[blockSelf will302RedirectToUrl:[request url]];
+		}
 	}];
 	
     if (!singleRequest)
