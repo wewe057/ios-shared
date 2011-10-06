636d7c13fe3bad7812cac7e9a7f8790f05878eef
Brandon Sneed <brandon@redf.net>
2011-10-06 11:03:03 -0700

    added uiview capturing method.

---
+++ UI/UIImage+SDExtensions.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+SDExtensions.h
+//  walmart
+//
+//  Created by Brandon Sneed on 10/5/11.
+//  Copyright (c) 2011 Walmart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (SDExtensions)
+
++ (UIImage *)imageFromView:(UIView *)view;
+
+@end
---
+++ UI/UIImage+SDExtensions.m
@@ -0,0 +1,29 @@
+//
+//  UIImage+SDExtensions.m
+//  walmart
+//
+//  Created by Brandon Sneed on 10/5/11.
+//  Copyright (c) 2011 Walmart. All rights reserved.
+//
+
+#import "UIImage+SDExtensions.h"
+
+@implementation UIImage (SDExtensions)
+
++ (UIImage *)imageFromView:(UIView *)view
+{
+    UIImage *result = nil;
+    
+    UIGraphicsBeginImageContextWithOptions(view.bounds.size, NO, 0.0);
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    [view.layer renderInContext:ctx];
+    for (CALayer *layer in view.layer.sublayers)
+    {
+        [layer renderInContext:ctx];
+    }
+    result = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return result;
+}
+
+@end
