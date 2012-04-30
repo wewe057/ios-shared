34751fbdec8040c4302a84dfd3e28e17fc93534e
Brandon Sneed <brandon@redf.net>
2012-04-30 10:14:47 -0700

    one more fix.

--- WebServices/NSCachedURLResponse+LeakFix.m
+++ WebServices/NSCachedURLResponse+LeakFix.m
@@ -13,21 +13,24 @@
 - (NSData *)responseData
 {
     NSData *result = nil;
-    
-    NSUInteger firstCount = CFGetRetainCount((__bridge CFTypeRef)self.data);
-    NSUInteger secondCount = CFGetRetainCount((__bridge CFTypeRef)self.data);
+    NSData *first = self.data;
+    NSData *second = self.data;
+    NSUInteger firstCount = CFGetRetainCount((__bridge CFTypeRef)first);
+    NSUInteger secondCount = CFGetRetainCount((__bridge CFTypeRef)second);
     result = self.data;
 
-    if (firstCount != secondCount)
+    if (first == second)
     {
-        // this os build has the leak...  commence bullshit.
-        
-        // release our 3 total accesses that incurred a retain.
-        CFRelease((__bridge CFTypeRef)result);
-        CFRelease((__bridge CFTypeRef)result);
-        CFRelease((__bridge CFTypeRef)result);
-    }
-    
+        if (firstCount != secondCount)
+        {
+            // this os build has the leak...  commence bullshit.
+            
+            // release our 3 total accesses that incurred a retain.
+            CFRelease((__bridge CFTypeRef)result);
+            CFRelease((__bridge CFTypeRef)result);
+            CFRelease((__bridge CFTypeRef)result);
+        }
+    }    
     return result;
 }
 
