c87f256f581b472ce440895755484b1c518596b9
mattreagan <silver529@gmail.com>
2012-05-18 18:06:23 -0700

    Added convenience comparison methods for NSDate

--- Foundation/NSDate+SDExtensions.h
+++ Foundation/NSDate+SDExtensions.h
@@ -14,4 +14,14 @@
 + (NSDate *)dateFromISO8601String:(NSString *)argDateString;
 + (NSDate *)dateFromRFC822String:(NSString *)argDateString;
 
+//Time interval comparison convenience:
+- (BOOL)happenedMoreThanNSecondsAgo:(int)numSeconds;
+- (BOOL)happenedMoreThanNMinutesAgo:(int)numMinutes;
+- (BOOL)happenedMoreThanNHoursAgo:(int)numHours;
+- (BOOL)happenedMoreThanNDaysAgo:(int)numDays;
+- (BOOL)happenedLessThanNSecondsAgo:(int)numSeconds;
+- (BOOL)happenedLessThanNMinutesAgo:(int)numMinutes;
+- (BOOL)happenedLessThanNHoursAgo:(int)numHours;
+- (BOOL)happenedLessThanNDaysAgo:(int)numDays;
+
 @end
