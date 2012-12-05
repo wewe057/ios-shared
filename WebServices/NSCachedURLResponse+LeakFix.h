668e4a627572d7fafc0074c0a441888528a40b39
Sam Grover <samgrover@gmail.com>
2012-12-04 16:37:29 -0800

    Small doc change

--- WebServices/NSCachedURLResponse+LeakFix.h
+++ WebServices/NSCachedURLResponse+LeakFix.h
@@ -12,7 +12,7 @@
 
 /**
  Workaround for a leak in iOS 5.0 and later when the data property is accessed.
- @warning **NOTE - ** iOS 5.0 and 5.1 have a leak when accessing NSCachedURLResponse.data.
+ @bug iOS 5.0 and 5.1 have a leak when accessing NSCachedURLResponse.data.
  At this time we haven't verified if iOS 6 and later have the issue too so this code runs for iOS 5 and later.
  If the data backed in the NSCachedURLResponse meets certain criteria the method will always return the same NSData but with its retain count incremented.
  In other cases, we will get back a new NSData.
