6de119321c00bafa1f010d8d3b27561044a475e5
Steven W. Riggins <steve@geeksrus.com>
2013-01-29 16:20:37 -0800

    IPHONEAPP-2876: Add URLByAppendingQueryString category method

---
+++ WebServices/NSURL+SDExtensions.h
@@ -0,0 +1,15 @@
+//
+//  NSURL+SDExtensions
+//  walmart
+//
+//  Created by Steven W. Riggins on 1/29/13.
+//  Copyright (c) 2013 Walmart. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSURL (SDExtensions)
+
+- (NSURL *)URLByAppendingQueryString:(NSString *)queryString;	// Experiment with namespace collisions was short lived
+
+@end
