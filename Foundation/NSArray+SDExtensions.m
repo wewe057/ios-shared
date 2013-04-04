0f0af7f3a0baa5b072b4dfb073238c9e42433ce0
Brandon Sneed <brandon@redf.net>
2013-04-04 10:06:53 -0700

    Fixed bug in callSelector.

--- Foundation/NSArray+SDExtensions.m
+++ Foundation/NSArray+SDExtensions.m
@@ -34,6 +34,7 @@
 - (void)callSelector:(SEL)aSelector argumentAddresses:(void *)arg1, ...
 {
     va_list args;
+    va_start(args, arg1);
     
     NSArray *items = [self copy];
     for (id object in items)
@@ -44,7 +45,6 @@
             NSInvocation *invocation = [NSInvocation invocationWithMethodSignature: methodSig];
             [invocation setTarget:object];
             [invocation setSelector:aSelector];
-            va_start(args, arg1);
             if (arg1)
                 [invocation setArgument:&arg1 atIndex:2];
             void *theArg = nil;
