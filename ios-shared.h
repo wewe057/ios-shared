96268de9402d6105117aae686f8461016d0fff92
Brandon Sneed <brandon@redf.net>
2013-04-17 11:19:04 -0700

    Added UIDevice+Machine.* required for Anivia.
    Signed-off-by: Brandon Sneed <brandon@redf.net>

--- ios-shared.h
+++ ios-shared.h
@@ -24,6 +24,7 @@
 #import "UIColor+SDExtensions.h"
 #import "UIScreen+SDExtensions.h"
 #import "UIView+SDExtensions.h"
+#import "UIDevice+machine.h"
 
 #import "SDURLConnection.h"
 #import "SDWebService.h"
