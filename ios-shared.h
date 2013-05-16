e00612be75e8da72f1fe627b231e1c5890192bdc
Brandon Sneed <brandon@redf.net>
2013-05-16 14:02:37 -0700

    Added UIViewController category w/ convenience methods for working with SDDeckController and SDContainerController.

--- ios-shared.h
+++ ios-shared.h
@@ -25,6 +25,7 @@
 #import "UIScreen+SDExtensions.h"
 #import "UIView+SDExtensions.h"
 #import "UIDevice+machine.h"
+#import "UIViewController+SDExtensions.h"
 
 #import "SDURLConnection.h"
 #import "SDWebService.h"
