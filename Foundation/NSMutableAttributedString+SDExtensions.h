a9a2b1a11fe7077d0cd0dd89ca4d166d8b7bf2dd
Steven W. Riggins <steve@geeksrus.com>
2013-08-13 22:44:26 -0700

    Add NSMutableAttributedString+SDExtensions for font and foreground color setters

---
+++ Foundation/NSMutableAttributedString+SDExtensions.h
@@ -0,0 +1,42 @@
+//
+//  NSMutableAttributedString+SDExtensions.h
+//  walmart
+//
+//  Created by Steven W. Riggins on 8/13/13.
+//  Copyright (c) 2013 Walmart. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSMutableAttributedString (SDExtensions)
+
+/**
+ *	Sets the foreground color of the attributed string
+ *
+ *	@param	color	The color of the text
+ */
+- (void)setForegroundColor:(UIColor *)color;
+
+/**
+ *	Sets the foreground color of the characters in the given range of the attributed string
+ *
+ *	@param	color	The color of the text
+ *	@param	range	The range to apply the color to
+ */
+- (void)setForegroundColor:(UIColor *)color range:(NSRange)range;
+
+/**
+ *	Sets the font of the attributed string
+ *
+ *	@param	font	The font of the text
+ */
+- (void)setFont:(UIFont *)font;
+
+/**
+ *	Sets the font of of the characters in the given range of the attributed string
+ *
+ *	@param	font	The font of the text
+ *	@param	range	The range to apply the font to
+ */
+- (void)setFont:(UIFont *)font range:(NSRange)range;
+@end
