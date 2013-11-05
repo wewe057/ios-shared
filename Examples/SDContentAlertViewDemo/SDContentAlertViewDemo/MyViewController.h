524bf40c36a09a64f5727cf7e33614012e6fc148
Brandon Sneed <brandon@redf.net>
2013-11-05 15:34:16 -0800

    Added SDContentAlertView and demo project in /Examples.

---
+++ Examples/SDContentAlertViewDemo/SDContentAlertViewDemo/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  SDContentAlertViewDemo
+//
+//  Created by Brandon Sneed on 11/4/13.
+//  Copyright (c) 2013 redf.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
---
+++ Examples/SDContentAlertViewDemo/SDContentAlertViewDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SDContentAlertViewDemo
+//
+//  Created by Brandon Sneed on 11/4/13.
+//  Copyright (c) 2013 redf.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MyAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MyAppDelegate class]));
+    }
+}
