a7266f8b988c77749bc4498017ed4b7269501073
Doug Pedley <dpedley@gmail.com>
2013-12-13 14:42:46 -0800

    new SDPickerView

---
+++ Source/UI/SDPickerView.h
@@ -0,0 +1,25 @@
+//
+//  SDPickerView.h
+//
+//  Created by Douglas Pedley on 12/13/13.
+//  Copyright (c) 2013 SetDirection. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ObjectiveCGenerics.h"
+
+GENERICSABLE(NSString)
+
+typedef void(^SDPickerViewDateCompletionBlock)(NSDate *selectedDate);
+typedef void(^SDPickerViewItemSelectionCompletionBlock)(int selectedItemIndex, NSString *selectedItem);
+
+@interface SDPickerView : UIButton
+
+-(void)configureAsDatePickerWithCompletion:(SDPickerViewDateCompletionBlock)completion;
+-(void)configureAsDatePicker:(NSDate *)initialDate completion:(SDPickerViewDateCompletionBlock)completion;
+-(void)configureAsDatePicker:(NSDate *)initialDate datePickerMode:(UIDatePickerMode)datePickerMode completion:(SDPickerViewDateCompletionBlock)completion;
+
+-(void)configureAsItemPicker:(NSArray <NSString>*)items completion:(SDPickerViewItemSelectionCompletionBlock)completion;
+-(void)configureAsItemPicker:(NSArray <NSString>*)items initialItem:(NSInteger)selectedItem completion:(SDPickerViewItemSelectionCompletionBlock)completion;
+
+@end
