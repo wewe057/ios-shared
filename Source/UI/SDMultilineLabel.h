f298c8218c872531e6ecd427bde4325b24292bae
Steven W. Riggins <steve@steveriggins.net>
2014-01-27 15:11:22 -0800

    Added SDMultilineLabel

---
+++ Source/UI/SDMultilineLabel.h
@@ -0,0 +1,14 @@
+//
+//  SDMultilineLabel.h
+//  LayoutTests
+//
+//  Created by Steve Riggins on 1/24/14.
+//  Copyright (c) 2014 SetDirection. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDMultilineLabel : UILabel
+
+
+@end
