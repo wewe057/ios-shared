525f60a36771de0b6be3b1814f1c4169eb3aba14
Tim Trautmann <timct@mac.com>
2014-01-28 17:52:28 -0800

    Adding commentary to Auto Layout Stack View

--- Source/UI/SDAutolayoutStackView.m
+++ Source/UI/SDAutolayoutStackView.m
@@ -10,6 +10,7 @@
 
 @implementation SDAutolayoutStackView
 
+// When a view has been added to this container, call updateConstraints.
 - (void)didAddSubview:(UIView *)subview
 {
     [super didAddSubview:subview];
@@ -19,6 +20,8 @@
 - (void)updateConstraints
 {
     [super updateConstraints];
+    
+    // Remove all constraints on this container view.
     [self removeConstraints:self.constraints];
     
     NSLayoutConstraint *bottomConstraint = nil;
@@ -26,6 +29,7 @@
     
     for (UIView *view in self.subviews)
     {
+        // If this is the topmost view, attach it to the parent view at the top.
         if ([self.subviews.firstObject isEqual:view])
         {
             [self addConstraint:[NSLayoutConstraint constraintWithItem:view
@@ -36,6 +40,7 @@
                                                             multiplier:1.0
                                                               constant:self.edgeInsets.top]];
         }
+        // Otherwise attach this view to the previous view's bottom.
         else
         {
             [self addConstraint:[NSLayoutConstraint constraintWithItem:view
@@ -47,15 +52,20 @@
                                                               constant:self.gap]];
         }
         
+        // All child views have leading and trailing constraints.
         [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(left)-[view]-(right)-|"
                                                                      options:0
                                                                      metrics:@{ @"left" : @(self.edgeInsets.left),
                                                                                 @"right" : @(self.edgeInsets.right) }
                                                                        views:NSDictionaryOfVariableBindings(view)]];
         
+        // If there is already a bottom constraint, remove it.
         if (bottomConstraint)
             [self removeConstraint:bottomConstraint];
         
+        
+        // Add a bottom constraint to attach view to the bottom of container
+        // view.
         bottomConstraint = [NSLayoutConstraint constraintWithItem:view
                                                         attribute:NSLayoutAttributeBottom
                                                         relatedBy:NSLayoutRelationEqual
@@ -65,6 +75,8 @@
                                                          constant:-self.edgeInsets.bottom];
         
         [self addConstraint:bottomConstraint];
+        
+        // Set this view to previousView for next iteration of this loop.
         previousView = view;
     }
 }
