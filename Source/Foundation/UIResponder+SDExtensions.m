4b3343695600b2e2e53146cb38fd8c5e1aea600c
Steven Woolgar <Steven_Woolgar@Woolsoft.com>
2014-02-17 11:03:27 -0800

    Changed the value of the constant. Added a comment to explain what exactly is going on with the currentFirstResponder and what NOT to do with it.

--- Source/Foundation/UIResponder+SDExtensions.m
+++ Source/Foundation/UIResponder+SDExtensions.m
@@ -6,16 +6,27 @@
 //  Copyright 2014 SetDirection. All rights reserved.
 //
 //  Based upon code by http://stackoverflow.com/users/1361982/vjk user.
+//
 
 #import "UIResponder+SDExtensions.h"
 #import <objc/message.h>
 
-static char const* const kSDFirstResponderKey = "first-responder";
+static char const* const kSDFirstResponderKey = "kSDFirstResponderKey";
 
 @implementation UIResponder(SDExtensions)
 
 /**
  Find the current first responder.
+
+ This little technique works around the lack of a way to determine the firstResponder.
+
+ sendAction: knows the firstResponder internally, so we send the first responder an action to call our method.
+ When it calls us, the supplied 'sender' variable is the firstResponder. Since we are a class extension, we have
+ no ivar to communicate back, so we use an associated object. Once back in the method, we pull that out, nil it
+ and return that value back to the caller.
+
+ NB: Do not store the returned pointer. Do your work with it without storing it otherwise you might be accessing
+ a pointer to an object that has been released.
  */
 
 - (instancetype)currentFirstResponder
