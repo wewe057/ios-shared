8524732b1199f2118851e74f7844e2d4f475a464
rcancro <rickycancro@gmail.com>
2014-04-30 14:26:49 -0700

    added SDExpandingTableViewController and example
    TODO: need to determine if UIPopoverController is still limiting width to < 600px.  The restriction seemed to go away in iOS 5ish, but the docs still say width should be < 600.

---
+++ Examples/SDExpandingTableView/SDExpandingTableView/SDViewController.h
@@ -0,0 +1,12 @@
+//
+//  SDViewController.h
+//  SDExpandingTableView
+//
+//  Created by ricky cancro on 4/23/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDViewController : UIViewController
+
+@end
