262d051124a9f4d674d76eb902a44c412c6f6bc1
Cody Garvin <cgarvin@walmartlabs.com>
2014-10-17 10:14:03 -0700

    CB-2295: Added the ability to calculate the height of a complex cell

---
+++ Source/UI/UITableViewCell+SDExtensions.m
@@ -0,0 +1,37 @@
+//
+//  UITableViewCell+SDExtensions.m
+//  walmart
+//
+//  Created by Cody Garvin on 10/17/14.
+//  Copyright (c) 2014 Walmart. All rights reserved.
+//
+
+#import "UITableViewCell+SDExtensions.h"
+
+@implementation UITableViewCell (SDExtensions)
+
+- (CGFloat)calculatedHeightForMultilineCellInTableview:(UITableView *)tableview
+{
+    [self setNeedsLayout];
+    [self layoutIfNeeded];
+    
+    self.bounds = CGRectMake(0.0f, 0.0f, CGRectGetWidth(tableview.bounds), CGRectGetHeight(tableview.bounds));
+    
+    CGFloat cellHeight = 44.0f;
+    if ([UIDevice systemMajorVersion] >= 8.0)
+    {
+        cellHeight = ([self systemLayoutSizeFittingSize:UILayoutFittingCompressedSize
+                                withHorizontalFittingPriority:UILayoutPriorityDefaultHigh
+                                      verticalFittingPriority:UILayoutPriorityFittingSizeLevel].height +
+                      [WalmartStyler tableViewCellSeparatorHeight]);
+    }
+    else
+    {
+        cellHeight = ([self systemLayoutSizeFittingSize:UILayoutFittingCompressedSize].height +
+                      [WalmartStyler tableViewCellSeparatorHeight]);
+    }
+
+    return cellHeight;
+}
+
+@end
