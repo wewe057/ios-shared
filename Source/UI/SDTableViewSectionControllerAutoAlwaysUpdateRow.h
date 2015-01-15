6bedd0a00a77865b564fc6bfe9cc9c8fb1b708bb
Wes Ostler <broderick99@gmail.com>
2015-01-15 14:31:10 -0800

    Revert "Revert "added SDTableViewSectionControllerAutoAlwaysUpdateRow subclass o..."

---
+++ Source/UI/SDTableViewSectionControllerAutoAlwaysUpdateRow.h
@@ -0,0 +1,13 @@
+//
+//  SDTableViewSectionControllerAutoAlwaysUpdateRow.h
+//  walmart
+//
+//  Created by Steve Cotner on 1/13/15.
+//  Copyright (c) 2015 Walmart. All rights reserved.
+//
+
+#import "SDTableViewSectionControllerAutoUpdateRow.h"
+
+@interface SDTableViewSectionControllerAutoAlwaysUpdateRow : SDTableViewSectionControllerAutoUpdateRow
+
+@end
