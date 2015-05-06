bb39b7b4edecd8f134f864a2f246249e2a68958c
David Pettigrew <dpettigrew@walmartlabs.com>
2015-05-06 16:27:29 -0700

    created a base REST API class category

---
+++ Source/Foundation/SDModelObject+REST.h
@@ -0,0 +1,21 @@
+//
+//  SDModelObject+SDModelObject_REST.h
+//  walmart
+//
+//  Created by David Pettigrew on 5/6/15.
+//  Copyright (c) 2015 Walmart. All rights reserved.
+//
+
+#import "SDModelObject.h"
+#import "SDWebService.h"
+
+@interface SDModelObject (REST)
+
+- (NSString *)serviceMethodNameForRequestId:(NSString *)requestId;
+
+// The model classes should implement this
+// It provides a dictionary of request names that reference requests in the service plist file
+// A Pangea subclass will override this and it "should" just work using new service requests in the plist file
+- (NSDictionary *)requestIdMap;
+
+@end
---
+++ Source/Foundation/SDModelObject+REST.m
@@ -0,0 +1,29 @@
+//
+//  SDModelObject+REST.m
+//  walmart
+//
+//  Created by David Pettigrew on 5/6/15.
+//  Copyright (c) 2015 Walmart. All rights reserved.
+//
+
+#import "SDModelObject+REST.h"
+#import "WalmartBaseService.h"
+
+@implementation SDModelObject (REST)
+
+#pragma mark Template method
+- (NSDictionary *)requestIdMap {
+    [NSException raise:@"Missing implementation" format:nil];
+    return @{};
+}
+
+// A Pangea subclass will override this and it "should" just work using new service requests in the plist file
+- (NSString *)serviceMethodNameForRequestId:(NSString *)requestId {
+    if (!requestId) {
+        return nil;
+    }
+    NSString *requestIdMapping = self.requestIdMap[requestId];
+    return requestIdMapping;
+}
+
+@end
