44b146ec86fd14c22fa60bd4d66f4fd29bc88d57
David Pettigrew <dpettigrew@walmartlabs.com>
2015-05-26 14:38:51 -0700

    removed check against SDK 8 since we no longer build with an earlier version of Xcode

--- Source/Mapping/SDLocationManager.m
+++ Source/Mapping/SDLocationManager.m
@@ -103,11 +103,8 @@ NSString *kSDLocationManagerHasReceivedLocationUpdateDefaultsKey = @"SDLocationM
 // Previous logic would return true if the status was kCLAuthorizationStatusNotDetermined
 // Which the code in this class relied on.  Changing that code to isLocationRejected
 - (BOOL)isLocationAllowed {
-    BOOL isLocationAllowed = (self.authorizationStatus == kCLAuthorizationStatusAuthorized);
-#ifdef __IPHONE_8_0
-    isLocationAllowed = isLocationAllowed || (self.authorizationStatus == kCLAuthorizationStatusAuthorizedAlways) ||
+    BOOL isLocationAllowed = (self.authorizationStatus == kCLAuthorizationStatusAuthorizedAlways) ||
                                             (self.authorizationStatus == kCLAuthorizationStatusAuthorizedWhenInUse);
-#endif
     return isLocationAllowed;
 }
 
@@ -260,25 +257,20 @@ NSString *kSDLocationManagerHasReceivedLocationUpdateDefaultsKey = @"SDLocationM
 
 - (void)requestAlwaysAuthorization
 {
-#ifdef __IPHONE_8_0
     // Must request permission here
     if ([UIDevice systemMajorVersion] >= 8.0)
     {
         [self.locationManager requestAlwaysAuthorization];
     }
-#endif
-
 }
 
 - (void)requestWhenInUseAuthorization
 {
-#ifdef __IPHONE_8_0
     // Must request permission here
     if ([UIDevice systemMajorVersion] >= 8.0)
     {
         [self.locationManager requestWhenInUseAuthorization];
     }
-#endif
 }
 
 
